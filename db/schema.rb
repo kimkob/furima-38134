a4c8cddd809c47ef6d5e46618a1a923ccb849370
kimkob <un1ql0.go0d@gmail.com>
2022-07-02 17:42:30 +0900

    ビュー作成まで

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_07_02_055054) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name", null: false
