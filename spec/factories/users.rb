7d9e1889066c75c49aa874e03be9f892c99ab01b
kimkob <un1ql0.go0d@gmail.com>
2022-07-04 17:46:43 +0900

    レビュー修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {Faker::Name}
-    email                   {Faker::Internet.free_email}
-    password                {Faker::Internet.password(min_length: 6)}
-    password_confirmation   {password}
-    family_name             {'山田'}
-    first_name              {'鉄お'}
-    family_kana             {'ヤマダ'}
-    first_kana              {'テツオ'}
-    birthday                {Faker::Date.birthday}
+    nickname                { Faker::Name }
+    email                   { 'a1' +Faker::Internet.free_email }
+    password                { Faker::Internet.password(min_length: 6)}
+    password_confirmation   { password }
+    family_name             { '山田' }
+    first_name              { '鉄お' }
+    family_kana             { 'ヤマダ' }
+    first_kana              { 'テツオ' }
+    birthday                { Faker::Date.birthday }
   end
 end
