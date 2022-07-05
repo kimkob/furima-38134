1f2ddbe7c3e0fe34b156194977203176399ec7e0
kimkob <un1ql0.go0d@gmail.com>
2022-07-05 17:12:43 +0900

    商品情報入力バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40}
+  validates :explain, presence: true, length: { maximum: 1000}
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :sender_area_id, presence: true
+  validates :delibery_days_id, presence: true
+  validates :price, presence: true
+
 end
