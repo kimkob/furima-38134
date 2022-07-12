b80183a71a5ec6a3f5ee75d91223677d2071dc00
kimkob <un1ql0.go0d@gmail.com>
2022-07-12 11:15:59 +0900

    formオブジェクト実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,25 @@
 class OrdersController < ApplicationController
-#  before_action :authenticate_user!, only: [:index, :create]
+#  before_action :authenticate_user!, only: [:create]
 
   def index
-    @order = Order.new
+    @order_shipping = OrderShipping.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-#    @order = Order.new(order_params)
-#    if @oder.valid?
-#      @order.save
-#      return redirect_to root_path
-#    else
-#      render :index
-#    end
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
-#  def order_params
-#    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
-#  end
+  def order_params
+    params.require(:order_shipping).permit(:post_code, :prefecture_id, :city_town, :address_2, :address_1, :tel, :item_id ).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,5 +18,5 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_time_id, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
 end
