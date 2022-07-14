adf0e415c07d9775288a2044d139bad6b086898e
kimkob <un1ql0.go0d@gmail.com>
2022-07-14 16:26:42 +0900

    カラムファイル修正

--- db/migrate/20220706064740_rename_sender_area_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameSenderAreaIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :sender_area_id, :prefecture_id
-  end
-end
--- db/migrate/20220706064958_rename_delivery_days_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameDeliveryDaysIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :delivery_days_id, :shipping_time_id
-  end
-end
